0bebc735578c75fa04f696085e7486faef55e63a
nicks9188 <nicks9188@gmail.com>
2019-03-05 20:36:58 +0530

    Added gitignore

--- Hello.java
+++ Hello.java
@@ -1,7 +1,10 @@
 public class Hello{
 	public static void main(String[] args){
-		for(int i=1;i<=10;i++){
-			System.out.println("Nicks..."+i);
+		for(int i=0;i<5;i++){
+			System.out.println(i);
+			for(int j=0;j<=i;j++){
+				System.out.println("*");
+			}
 		}
 	}
 }
