09ebebff9c3f68dc285455236cdc0b7518108212
nicks9188 <nicks9188@gmail.com>
2019-03-05 21:03:09 +0530

    Updated code. Added loop

--- Hello.java
+++ Hello.java
@@ -1,10 +1,16 @@
-public class Hello{
-	public static void main(String[] args){
-		for(int i=0;i<5;i++){
-			System.out.println(i);
-			for(int j=0;j<=i;j++){
-				System.out.println("*");
-			}
-		}
-	}
+public class Hello {
+    public static void main(String[] args) {
+
+        simplePyramind(5);
+    }
+
+    public static void simplePyramind(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print("Hello");
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" Java");   //print instead of println
+            }
+            System.out.println();
+        }
+    }
 }
